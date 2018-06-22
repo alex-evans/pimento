62ef562305c69cf41173bef29ca32317e8df61f9
Alex Evans <alexevans@sansoros-mbp-3.lan>
2018-06-22 15:27:39 -0500

    Moved thigns around

--- server/server.py
+++ server/server.py
@@ -1,25 +1,20 @@
-import random
-from flask import Flask, render_template
+# server.py
+from flask import Flask, render_template, jsonify
+from web_scrape import Event
 
-app = Flask(__name__, static_folder='../static/dist', template_folder='../static')
+#app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
+app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/hello') # take note of this decorator syntax, it's a common pattern
-def hello():
-    # It is good practice to only call a function in your route end-point,
-    # rather than have actual implementation code here.
-    # This allows for easier unit and integration testing of your functions.
-    return get_hello()
+@app.route("/event")
+def event():
+    event = Event()
+    return jsonify(event.event_group_a) 
 
 
-def get_hello():
-    greeting_list = ['Ciao', 'Hei', 'Salut', 'Hola', 'Hallo', 'Hej']
-    return random.choice(greeting_list)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
